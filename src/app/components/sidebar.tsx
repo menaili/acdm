140d128f1839ac312ab17f69cc08bd550a5484a4
Menaili Haroun Errachide <menailimic@gmail.com>
2024-03-07 11:18:31 -0800

    Add sidebar components

---
+++ src/app/components/sidebar.tsx
@@ -0,0 +1,99 @@
+import Image from "next/image";
+
+const Sidebar: React.FC = () => {
+    return(
+        <div className="flex flex-col justify-around items-center fixed bg-blue-950 h-screen w-16 p-1">
+            <div>
+                <Image
+                    src={'/statics/logo/mortarboard.png'}
+                    width={40}
+                    height={40} 
+                    alt={"Logo"}                
+                />
+            </div>
+
+            <div className="flex flex-col justify-between">
+                <div className="my-1 p-1 rounded-lg hover:bg-sky-800">
+                    <Image
+                        src={'/statics/menu/app.png'}
+                        width={40}
+                        height={40} 
+                        alt={"Logo"}
+                        style={{ filter: 'brightness(0) invert(1)' }} 
+                    />
+                </div>
+
+                <div className="my-1 p-1 rounded-lg hover:bg-sky-800">
+                    <Image
+                        src={'/statics/menu/app.png'}
+                        width={40}
+                        height={40} 
+                        alt={"Logo"}
+                        style={{ filter: 'brightness(0) invert(1)' }} 
+                    />
+                </div>
+
+                <div className="my-1 p-1 rounded-lg hover:bg-sky-800">
+                    <Image
+                        src={'/statics/menu/app.png'}
+                        width={40}
+                        height={40} 
+                        alt={"Logo"}
+                        style={{ filter: 'brightness(0) invert(1)' }} 
+                    />
+                </div>
+
+                <div className="my-1 p-1 rounded-lg hover:bg-sky-800">
+                    <Image
+                        src={'/statics/menu/app.png'}
+                        width={40}
+                        height={40} 
+                        alt={"Logo"}
+                        style={{ filter: 'brightness(0) invert(1)' }} 
+                    />
+                </div>
+
+                <div className="my-1 p-1 rounded-lg hover:bg-sky-800">
+                    <Image
+                        src={'/statics/menu/app.png'}
+                        width={40}
+                        height={40} 
+                        alt={"Logo"}
+                        style={{ filter: 'brightness(0) invert(1)' }} 
+                    />
+                </div>
+
+                <div className="my-1 p-1 rounded-lg hover:bg-sky-800">
+                    <Image
+                        src={'/statics/menu/app.png'}
+                        width={40}
+                        height={40} 
+                        alt={"Logo"}
+                        style={{ filter: 'brightness(0) invert(1)' }} 
+                    />
+                </div>
+
+                <div className="my-1 p-1 rounded-lg hover:bg-sky-800">
+                    <Image
+                        src={'/statics/menu/app.png'}
+                        width={40}
+                        height={40} 
+                        alt={"Logo"}
+                        style={{ filter: 'brightness(0) invert(1)' }} 
+                    />
+                </div>
+            </div>
+            <div>
+            <Image
+                    src={'/profile/portrait-man-laughing.jpg'}
+                    width={40}
+                    height={40}
+                    alt={"profile"}    
+                    className="rounded-full"            
+                />
+            </div>
+        </div>
+    )
+}
+
+export default Sidebar;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,15 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      
+    <main className="flex min-h-screen flex-col items-center bg-white-500 justify-between p-24">
+      <div className="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-lg flex items-center space-x-4">
+        <div className="shrink-0">
+        </div>
+        <div>
+          <div className="text-xl font-medium text-black">ChitChat</div>
+          <p className="text-slate-500">You have a new message!</p>
+        </div>
+      </div>
     </main>
   );
 }
