9f6232ab8c6e02c898857cc99c137fb40b7fae5e
Menaili Haroun Errachide <menailimic@gmail.com>
2024-03-10 15:39:30 -0700

    Add icons ti sidebar

--- src/app/components/sidebar.tsx
+++ src/app/components/sidebar.tsx
@@ -13,12 +13,12 @@ const Sidebar: React.FC = () => {
                 />
             </div>
 
-            <div className="flex flex-col justify-between">
+            <div className="flex flex-col justify-between gap-3">
                 <div className="my-1 p-1 rounded-lg hover:bg-hover">
                     <Image
                         src={'/statics/menu/app.png'}
-                        width={40}
-                        height={40} 
+                        width={30}
+                        height={30} 
                         alt={"Logo"}
                         style={{ filter: 'brightness(0) invert(1)' }} 
                     />
@@ -26,9 +26,30 @@ const Sidebar: React.FC = () => {
 
                 <div className="my-1 p-1 rounded-lg hover:bg-hover">
                     <Image
-                        src={'/statics/menu/app.png'}
-                        width={40}
-                        height={40} 
+                        src={'/statics/menu/people.png'}
+                        width={30}
+                        height={30} 
+                        alt={"Logo"}
+                        style={{ filter: 'brightness(0) invert(1)' }} 
+                    />
+                </div>
+
+                <div className="my-1 p-1 rounded-lg hover:bg-hover relative">
+                    <Image
+                        src={'/statics/menu/notification.png'}
+                        width={30}
+                        height={30} 
+                        alt={"Logo"}
+                        style={{ filter: 'brightness(0) invert(1)' }} 
+                    />
+                    <div className="absolute top-2 right-2 w-3 h-3 bg-red rounded-full"></div>
+                </div>
+
+                <div className="my-1 p-1 rounded-lg hover:bg-hover">
+                    <Image
+                        src={'/statics/menu/message.png'}
+                        width={30}
+                        height={30} 
                         alt={"Logo"}
                         style={{ filter: 'brightness(0) invert(1)' }} 
                     />
@@ -36,9 +57,9 @@ const Sidebar: React.FC = () => {
 
                 <div className="my-1 p-1 rounded-lg hover:bg-hover">
                     <Image
-                        src={'/statics/menu/app.png'}
-                        width={40}
-                        height={40} 
+                        src={'/statics/menu/list.png'}
+                        width={30}
+                        height={30} 
                         alt={"Logo"}
                         style={{ filter: 'brightness(0) invert(1)' }} 
                     />
@@ -46,39 +67,9 @@ const Sidebar: React.FC = () => {
 
                 <div className="my-1 p-1 rounded-lg hover:bg-hover">
                     <Image
-                        src={'/statics/menu/app.png'}
-                        width={40}
-                        height={40} 
-                        alt={"Logo"}
-                        style={{ filter: 'brightness(0) invert(1)' }} 
-                    />
-                </div>
-
-                <div className="my-1 p-1 rounded-lg hover:bg-hover">
-                    <Image
-                        src={'/statics/menu/app.png'}
-                        width={40}
-                        height={40} 
-                        alt={"Logo"}
-                        style={{ filter: 'brightness(0) invert(1)' }} 
-                    />
-                </div>
-
-                <div className="my-1 p-1 rounded-lg hover:bg-hover">
-                    <Image
-                        src={'/statics/menu/app.png'}
-                        width={40}
-                        height={40} 
-                        alt={"Logo"}
-                        style={{ filter: 'brightness(0) invert(1)' }} 
-                    />
-                </div>
-
-                <div className="my-1 p-1 rounded-lg hover:bg-hover">
-                    <Image
-                        src={'/statics/menu/app.png'}
-                        width={40}
-                        height={40} 
+                        src={'/statics/menu/settings.png'}
+                        width={30}
+                        height={30} 
                         alt={"Logo"}
                         style={{ filter: 'brightness(0) invert(1)' }} 
                     />
