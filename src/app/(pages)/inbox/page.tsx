4c103cf3e974e1729456d94465514e4fc2ea0cc9
Menaili Haroun Errachide <menailimic@gmail.com>
2024-03-12 10:18:46 +0100

    Update page.tsx

--- src/app/(pages)/inbox/page.tsx
+++ src/app/(pages)/inbox/page.tsx
@@ -4,7 +4,7 @@ import Searchbar from "@/components/single/search";
 
 export default function Home() {
     return (
-        <main className="flex min-h-screen flex-row bg-white-500 gap-5 px-24 pb-32">
+        <main className="flex max-h-screen min-h-screen flex-row bg-white-500 gap-5 px-24 pb-32  overflow-y-hidden">
             <div className="flex flex-col w-fit rounded-xl border border-opacity-40 border-solid border-gray">
                 <Messages />
             </div>
